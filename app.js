8f9942471a3e06af5db99c778bc65ee4005cc3de
tes-kid <mikari.ouju+github2020@gmail.com>
2020-05-18 19:25:02 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+function fib(n) {
+    if (n === 0){
+        return 0;
+    } else if (n === 1){
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+
+//console.log(fib(0));
+//console.log(fib(1));
+//console.log(fib(3));
